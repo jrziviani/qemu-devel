de219f29220c9b966c3f7e239885b45fa5dac65f
Jianbo Liu <jianbol@mellanox.com>
2018-03-27 09:22:16 +0000

    net/mlx5e: Allow offloading ipv4 header re-write for icmp
    For ICMPv4, the checksum is calculated from the ICMP headers and data.
    Since the ICMPv4 checksum doesn't cover the IP header, we can allow to
    do L3 header re-write for this protocol.
    
    Issue: 1332238
    Change-Id: I8433709bb60ca08d2e42de4eac44a700a697981d
    Fixes: bdd66ac0aeed ('net/mlx5e: Disallow TC offloading of unsupported match/action combinations')
    Signed-off-by: Jianbo Liu <jianbol@mellanox.com>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    [juergh: IBM patchset mlx-4.15.0-24.26-patches-20180921 (SF #00199436).]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -1345,7 +1345,7 @@ out_err:
 static bool csum_offload_supported(struct mlx5e_priv *priv, u32 action, u32 update_flags)
 {
 	u32 prot_flags = TCA_CSUM_UPDATE_FLAG_IPV4HDR | TCA_CSUM_UPDATE_FLAG_TCP |
-			 TCA_CSUM_UPDATE_FLAG_UDP;
+			 TCA_CSUM_UPDATE_FLAG_UDP | TCA_CSUM_UPDATE_FLAG_ICMP;
 
 	/*  The HW recalcs checksums only if re-writing headers */
 	if (!(action & MLX5_FLOW_CONTEXT_ACTION_MOD_HDR)) {
