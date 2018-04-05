90ee0565827ea7c32e1a5c298d0084f1a00f8f6a
Or Gerlitz <ogerlitz@mellanox.com>
2018-04-05 14:55:39 +0300

    net/mlx5e: Return success when TC offloaded fdb actions parsed ok
    Reaching here, means we didn't err anywhere, so lets just
    return success.
    
    Issue: 1356144
    Change-Id: Ia5ffd54946d4c0133137f85bc25de1a6b06b3273
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Reviewed-by: Jianbo Liu <jianbol@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    
    [juergh: IBM patchset mlx-4.15.0-24.26-patches-20180921 (SF #00199436).]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -1967,7 +1967,6 @@ static int parse_tc_fdb_actions(struct mlx5e_priv *priv, struct tcf_exts *exts,
 	const struct tc_action *a;
 	LIST_HEAD(actions);
 	bool encap = false;
-	int err = 0;
 	u32 action = 0;
 
 	if (!tcf_exts_has_actions(exts))
@@ -1984,6 +1983,8 @@ static int parse_tc_fdb_actions(struct mlx5e_priv *priv, struct tcf_exts *exts,
 		}
 
 		if (is_tcf_pedit(a)) {
+			int err;
+
 			err = parse_tc_pedit_action(priv, a, MLX5_FLOW_NAMESPACE_FDB,
 						    parse_attr);
 			if (err)
@@ -2075,7 +2076,7 @@ static int parse_tc_fdb_actions(struct mlx5e_priv *priv, struct tcf_exts *exts,
 	if (!actions_match_supported(priv, exts, parse_attr, flow))
 		return -EOPNOTSUPP;
 
-	return err;
+	return 0;
 }
 
 int mlx5e_configure_flower(struct mlx5e_priv *priv,
