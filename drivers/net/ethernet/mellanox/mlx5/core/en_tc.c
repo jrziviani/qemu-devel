3675133cfc84ac74c8d696a3d4943ba1bd6616bd
Paul Blakey <paulb@mellanox.com>
2018-05-24 07:38:11 +0300

    WIP - for compilation
    fix backports for compilation
    
    Change-Id: I68a7297e301d3d60d0c03784cdd683ade3c21b2b
    
    [juergh: IBM patchset mlx-4.15.0-24.26-patches-20180921 (SF #00199436).]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -2098,8 +2098,7 @@ static int mlx5e_route_lookup_ipv4(struct mlx5e_priv *priv,
 	int ret;
 
 	if (mlx5_lag_is_multipath_ready(esw->dev)) {
-		uplink_rpriv = mlx5_eswitch_get_uplink_priv(esw, REP_ETH);
-		*out_dev = uplink_rpriv->netdev;
+		*out_dev = mlx5_eswitch_get_uplink_netdev(esw);
 		fl4->flowi4_oif = (*out_dev)->ifindex;
 	}
 
@@ -2700,7 +2699,7 @@ static struct rhashtable *get_tc_ht(struct mlx5e_priv *priv)
 	struct mlx5e_rep_priv *uplink_rpriv;
 
 	if (MLX5_VPORT_MANAGER(priv->mdev) && esw->mode == SRIOV_OFFLOADS) {
-		uplink_rpriv = mlx5_eswitch_get_uplink_priv(esw, REP_ETH);
+		uplink_rpriv = mlx5_eswitch_get_uplink_netdev_priv(esw, REP_ETH);
 		return &uplink_rpriv->tc_ht;
 	} else
 		return &priv->fs.tc.ht;
