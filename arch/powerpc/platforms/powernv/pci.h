e0702a6dccb902d0b9357abfcc1e75e90827f9a5
Alexey Kardashevskiy <aik@ozlabs.ru>
2018-09-26 17:30:19 +1000

    FIXME: powerpc/powernv: Detach npu struct from pnv_phb
    BugLink: https://bugs.launchpad.net/bugs/1800649
    
    The powernv PCI code stores NPU data in the pnv_phb struct reference to
    which is stored in pci_controller::private_data.
    
    We are going to have NPU2 support in pseries platform as well but it does
    not store any private_data in in the pci_controller struct.
    
    This adds a global list of NPUs so each platform can register and use
    these in the same fashion.
    
    This moves pnv_npu2_init() declaration out of the powernv folder so
    later pseries could include it; this also renames it.
    
    This moves LPAR mapping to a separate helper as later we are going to
    call pnv_npu2_init() from the pseries platform and LPAR is going to get
    mapped differently.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Jose Ricardo Ziviani <joserz@linux.ibm.com>
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- arch/powerpc/platforms/powernv/pci.h
+++ arch/powerpc/platforms/powernv/pci.h
@@ -8,9 +8,6 @@
 
 struct pci_dn;
 
-/* Maximum possible number of ATSD MMIO registers per NPU */
-#define NV_NMMU_ATSD_REGS 8
-
 enum pnv_phb_type {
 	PNV_PHB_IODA1		= 0,
 	PNV_PHB_IODA2		= 1,
@@ -180,19 +177,6 @@ struct pnv_phb {
 	unsigned int		diag_data_size;
 	u8			*diag_data;
 
-	/* Nvlink2 data */
-	struct npu {
-		int index;
-		__be64 *mmio_atsd_regs[NV_NMMU_ATSD_REGS];
-		unsigned int mmio_atsd_count;
-
-		/* Bitmask for MMIO register usage */
-		unsigned long mmio_atsd_usage;
-
-		/* Do we need to explicitly flush the nest mmu? */
-		bool nmmu_flush;
-	} npu;
-
 	int p2p_target_count;
 };
 
@@ -240,7 +224,7 @@ extern long pnv_npu_set_window(struct pnv_ioda_pe *npe, int num,
 extern long pnv_npu_unset_window(struct pnv_ioda_pe *npe, int num);
 extern void pnv_npu_take_ownership(struct pnv_ioda_pe *npe);
 extern void pnv_npu_release_ownership(struct pnv_ioda_pe *npe);
-extern int pnv_npu2_init(struct pnv_phb *phb);
+extern void pnv_npu2_map_lpar_phb(struct pnv_phb *nphb, unsigned long msr);
 
 /* phb ops (cxl switches these when enabling the kernel api on the phb) */
 extern const struct pci_controller_ops pnv_cxl_cx4_ioda_controller_ops;
