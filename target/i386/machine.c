b95483e9a18050c7dac0e6c17b049f0733a409cd
Miroslav Rezanina <mrezanin@redhat.com>
2018-11-08 12:03:11 +0100

    Add x86_64 machine types
    Adding changes to add RHEL machine types for x86_64 architecture.
    
    Signed-off-by: Miroslav Rezanina <mrezanin@redhat.com>

--- target/i386/machine.c
+++ target/i386/machine.c
@@ -955,6 +955,26 @@ static const VMStateDescription vmstate_svm_npt = {
     }
 };
 
+static bool vmstate_xsave_needed(void *opaque)
+{
+    /* The xsave state is already on the main "cpu" section */
+    return false;
+}
+
+static const VMStateDescription vmstate_xsave ={
+    .name = "cpu/xsave",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .minimum_version_id_old = 1,
+    .needed = vmstate_xsave_needed,
+    .fields      = (VMStateField []) {
+        VMSTATE_UINT64_V(env.xcr0, X86CPU, 1),
+        VMSTATE_UINT64_V(env.xstate_bv, X86CPU, 1),
+        VMSTATE_YMMH_REGS_VARS(env.xmm_regs, X86CPU, CPU_NB_REGS, 1),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
 VMStateDescription vmstate_x86_cpu = {
     .name = "cpu",
     .version_id = 12,
@@ -1080,6 +1100,7 @@ VMStateDescription vmstate_x86_cpu = {
         &vmstate_msr_intel_pt,
         &vmstate_msr_virt_ssbd,
         &vmstate_svm_npt,
+        &vmstate_xsave,
         NULL
     }
 };
