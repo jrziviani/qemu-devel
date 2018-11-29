521b1312749f12211b34f774d5981e2d3936dd62
Raed Salem <raeds@mellanox.com>
2018-11-29 12:02:25 +0200

    IB/mlx5: E-Switch, avoid port remapping of mlx5 IB dev TISes
    when at switchdev mode, TISes belonging to the mlx5 IB NIC should
    not be subject to port remap.
    
    Signed-off-by: Raed Salem <raeds@mellanox.com>
    
    [juergh: IBM patchset mlx-4.15.0-24.26-patches-20181210 (SF #00208194).]
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -1068,12 +1068,21 @@ static int create_raw_packet_qp_tis(struct mlx5_ib_dev *dev,
 				    struct mlx5_ib_qp *qp,
 				    struct mlx5_ib_sq *sq, u32 tdn)
 {
+#ifdef CONFIG_MLX5_ESWITCH
+	struct mlx5_eswitch *esw = dev->mdev->priv.eswitch;
+#endif
 	u32 in[MLX5_ST_SZ_DW(create_tis_in)] = {0};
 	void *tisc = MLX5_ADDR_OF(create_tis_in, in, ctx);
 
 	MLX5_SET(tisc, tisc, transport_domain, tdn);
 	if (qp->flags & MLX5_IB_QP_UNDERLAY)
 		MLX5_SET(tisc, tisc, underlay_qpn, qp->underlay_qpn);
+
+#ifdef CONFIG_MLX5_ESWITCH
+	if (mlx5_core_is_pf(dev->mdev) &&
+	    mlx5_eswitch_mode(esw) == SRIOV_OFFLOADS)
+		MLX5_SET(tisc, tisc, strict_lag_tx_port_affinity, 1);
+#endif
 
 	return mlx5_core_create_tis(dev->mdev, in, sizeof(in), &sq->tisn);
 }
