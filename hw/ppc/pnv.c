73dfeb2c4cd0145351c72092826591b8af9c359f
Joel Stanley <joel@jms.id.au>
2019-07-18 15:02:36 +0930

    ppc/pnv: Set default ram size to 2GB
    This makes the powernv machine easier for end users as the default
    initrd address is now within RAM.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Message-Id: <20190718053236.6721-1-joel@jms.id.au>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/pnv.c
+++ hw/ppc/pnv.c
@@ -1358,7 +1358,7 @@ static void pnv_machine_class_init(ObjectClass *oc, void *data)
                                       * storage */
     mc->no_parallel = 1;
     mc->default_boot_order = NULL;
-    mc->default_ram_size = 1 * GiB;
+    mc->default_ram_size = 2 * GiB;
     xic->icp_get = pnv_icp_get;
     xic->ics_get = pnv_ics_get;
     xic->ics_resend = pnv_ics_resend;
