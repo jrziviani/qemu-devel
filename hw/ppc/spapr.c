c1623c57897dd5a43cd92ba2a4617d97c587a1a2
David Gibson <david@gibson.dropbear.id.au>
2019-09-26 16:12:05 +1000

    spapr, xics, xive: Move print_info from SpaprIrq to SpaprInterruptController
    This method depends only on the active irq controller.  Now that we've
    formalized the notion of active controller we can dispatch directly
    through that, rather than dispatching via SpaprIrq with the dual
    version having to do a second conditional dispatch.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    (cherry picked from commit 328d8eb24db8ec415260ee7243adf2e3d7e81bad)
    
    Conflicts:
    	hw/ppc/spapr.c
    
    Contextual conflict because we don't have f041d6af557 "spapr: Report
    kvm_irqchip_in_kernel() in 'info pic'" downstream.
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1739299
    
    Signed-off-by: David Gibson <dgibson@redhat.com>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -4279,7 +4279,7 @@ static void spapr_pic_print_info(InterruptStatsProvider *obj,
 {
     SpaprMachineState *spapr = SPAPR_MACHINE(obj);
 
-    spapr->irq->print_info(spapr, mon);
+    spapr_irq_print_info(spapr, mon);
 }
 
 int spapr_get_vcpu_id(PowerPCCPU *cpu)
