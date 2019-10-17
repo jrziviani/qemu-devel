7af8c30576fb934f24b27161fe944845f433c835
David Gibson <david@gibson.dropbear.id.au>
2019-10-17 12:12:35 +1100

    kvm: Introduce KVM irqchip change notifier
    Awareness of an in kernel irqchip is usually local to the machine and its
    top-level interrupt controller.  However, in a few cases other things need
    to know about it.  In particular vfio devices need this in order to
    accelerate interrupt delivery.
    
    If interrupt routing is changed, such devices may need to readjust their
    connection to the KVM irqchip.  pci_bus_fire_intx_routing_notifier() exists
    to do just this.
    
    However, for the pseries machine type we have a situation where the routing
    remains constant but the top-level irq chip itself is changed.  This occurs
    because of PAPR feature negotiation which allows the guest to decide
    between the older XICS and newer XIVE irq chip models (both of which are
    paravirtualized).
    
    To allow devices like vfio to adjust to this change, introduce a new
    notifier for the purpose kvm_irqchip_change_notify().
    
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Alexey Kardashevskiy <aik@ozlabs.ru>
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Tested-by: Alex Williamson <alex.williamson@redhat.com>
    Reviewed-by: Alex Williamson <alex.williamson@redhat.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Acked-by: Alex Williamson <alex.williamson@redhat.com>
    (cherry picked from commit 3607715a308d94b8c4797d23620e268a4c5bebf6)
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1739299
    
    Signed-off-by: David Gibson <dgibson@redhat.com>

--- include/sysemu/kvm.h
+++ include/sysemu/kvm.h
@@ -202,6 +202,7 @@ typedef struct KVMCapabilityInfo {
 struct KVMState;
 typedef struct KVMState KVMState;
 extern KVMState *kvm_state;
+typedef struct Notifier Notifier;
 
 /* external API */
 
@@ -401,6 +402,10 @@ int kvm_set_irq(KVMState *s, int irq, int level);
 int kvm_irqchip_send_msi(KVMState *s, MSIMessage msg);
 
 void kvm_irqchip_add_irq_route(KVMState *s, int gsi, int irqchip, int pin);
+
+void kvm_irqchip_add_change_notifier(Notifier *n);
+void kvm_irqchip_remove_change_notifier(Notifier *n);
+void kvm_irqchip_change_notify(void);
 
 void kvm_get_apic_state(DeviceState *d, struct kvm_lapic_state *kapic);
 
